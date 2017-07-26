5c4d9a96ddf144e4e31bfc99ac930f29cf8c7aff
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-07-26 13:13:30 +0200

    Done the header and footer, and arranged the routes
    A test page was created to check that the header and footer will show
    and that th page load as needed.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/StudentCompanion/PHP-Synoptic/';
 
 /*
 |--------------------------------------------------------------------------
@@ -114,7 +114,7 @@ $config['enable_hooks'] = FALSE;
 | https://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'MY_';
+$config['subclass_prefix'] = 'SC_';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'S2den7..c()mpan10n';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/struct/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Student Companion</title>
+
+        <link type="text/css" rel="stylesheet" href="../css/style.css">
+    </head>
+
+    <body>
+
+        <naV id="menu">
+            <ul>
+                <li>
+                    <a href="#">News Feed</a>
+                </li>
+
+                <li>
+                    <a href="#">Profile Page</a>
+                </li>
+
+                <li>
+                    <a href="#">Notes</a>
+                </li>
+
+                <li>
+                    <a href="#">Contact Form</a>
+                </li>
+
+                <hr>
+
+                <li>
+                    <a href="#">Log Out</a>
+                </li>
+
+            </ul>
+        </naV>
+
+        <header id="title">
+            <h1>Student Companion</h1>
+        </header>
+
+        <main>
